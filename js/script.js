0b5ea227a2d8e4d95e5c4d48cf158017a1e2bf23
Ratan Mondal <101461464+ratanmondal1@users.noreply.github.com>
2022-07-28 11:38:51 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $('.bannar-content').animatedHeadline({
+  animationType: "clip",
+  animationDelay: 2500,
+  barAnimationDelay: 3800,
+  barWaiting: 800,
+  lettersDelay: 50,
+  typeLettersDelay: 100,
+  selectionDuration: 500,
+  typeAnimationDelay: 1300,
+  revealDuration: 600,
+  revealAnimationDelay: 1500
+});
+})
+
